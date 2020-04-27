d2092f68604f22cae29fd337f52bd543822ca466
sujoNLt <nslaustas@gmail.com>
2020-04-27 21:15:20 +0300

    Prideti trys interface

---
+++ Zmogus/Zmogus/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zmogus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    interface IDuomenys
+    {
+        int Metai { get; }
+        string Vardas { get; }
+        double Ugis { get; set; }
+    }
+
+    interface IKomunikacija
+    {
+        string Kalbeti(string zodziai);
+        string Rasyti(string tekstas);
+    }
+
+    interface IKeliavimas
+    {
+        void Ejimas();
+    }
+}
